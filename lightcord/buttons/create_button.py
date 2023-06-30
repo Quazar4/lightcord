2323dcddce8c259a25712e0d35944c5e28caa9f1
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-06-30 21:28:22 +0530

    Update create_button.py

--- lightcord/buttons/create_button.py
+++ lightcord/buttons/create_button.py
@@ -1,13 +1,16 @@
-import json
-import websockets
+import aiohttp
 
-async def create_button(websocket, channel_id, message_id, label):
-    payload = {
-        "op": 6,
-        "d": {
-            "channel_id": channel_id,
-            "message_id": message_id,
-            "label": label
-        }
+async def create_button(token, channel_id, message_id, label):
+    url = f"https://discord.com/api/v10/channels/{channel_id}/messages/{message_id}/buttons"
+    headers = {
+        "Authorization": f"Bot {token}",
+        "Content-Type": "application/json"
     }
-    await websocket.send(json.dumps(payload))
+    payload = {
+        "label": label
+    }
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers, json=payload) as response:
+            if response.status != 201:
+                raise Exception(f"Failed to create button: {response.status}")
